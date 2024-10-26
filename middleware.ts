d25cf98a9acea697682da54365db30c87aa44911
Flawless <admin@flawless.run>
2024-10-26 14:05:22 +0200

    chore: upate matcher config & assign reused checks to variables

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,31 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest, res: NextResponse) {
+  const supabase = createMiddlewareClient({ req, res });
 
   const {
     data: { session },
-  } = await supabase.auth.getSession()
+  } = await supabase.auth.getSession();
 
-  // If there's no session and the user is not trying to access the auth page, redirect to /auth
-  if (!session && req.nextUrl.pathname !== '/auth') {
-    return NextResponse.redirect(new URL('/auth', req.url))
+  const isAuthPage = req.nextUrl.pathname === "/auth";
+  const isAuthed = !!session;
+
+  if (!isAuthed && !isAuthPage) {
+    return NextResponse.redirect(new URL("/auth", req.url));
   }
 
-  // If there's a session and the user is trying to access the auth page, redirect to /
-  if (session && req.nextUrl.pathname === '/auth') {
-    return NextResponse.redirect(new URL('/', req.url))
+  if (isAuthed && isAuthPage) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
-  return res
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
